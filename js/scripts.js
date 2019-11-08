3b7b540a733ce2c863323eebcf923b154200e92e
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-08 10:03:00 -0800

    add scripts and html

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Backend Logic
+function Pizza(size, toppings) {
+  this.toppings = toppings,
+  this.size = size,
+
+}
+
+Pizza.prototype.pizzaPrice = function(price) {
+
+}
+
+
+
+// Frontend Logic
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+
+
+    
+  })
+})
