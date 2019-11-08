33c87dcd9706727f9a89435ea83bf29f135fbc3a
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-08 12:37:01 -0800

    add more branching

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.pizzaPrice = function(price) {
-
+  if (this.size === "small"){
+    this.price += 10;
+  } else if (this.size === "medium"){
+    this.price += 15;
+  } else if (this.size === "large"){
+    this.price += 20;
+  } else if (this.toppings.length > 2){
+    this.price += 1;
+  } else if (this.toppings.length < 2){
+    this.price += 0;
+  }
 }
 
+if (this)
 
 
 // Frontend Logic
@@ -18,6 +29,6 @@ $(document).ready(function(){
 
 
 
-    
+
   })
 })
