d157b877cabcf80525423850b16dca33fb323956
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-17 20:21:31 -0800

    add if else and radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,57 @@
 // Backend Logic
-function Pizza(size, toppings) {
+
+function Pizza(size, toppings, price) {
   this.toppings = toppings,
   this.size = size,
-  this.price = 0
+  this.price = 10
 }
 
-Pizza.prototype.pizzaPrice = function(price) {
-  if (this.size === "small"){
-    return this.price += 10;
-  } else if (this.size === "medium"){
-    return this.price += 15;
-  } else if (this.size === "large"){
-    return this.price += 20;
-  } else if (this.toppings.length > 2){
-    return this.price += 1;
-  } else if (this.toppings.length < 2){
-    return this.price += 0;
-  }
-  return size, toppings;
+function Order(){
+  this.order = [];
+  this.finalOrder = 0;
 }
-console.log(this.size);
+
+Pizza.prototype.getFee = function () {
+  var fee = 0;
+  if (this.size === "small"){
+    cost = size + 10;
+    console.log(size);
+  } else if (this.size === "medium"){
+    cost = size + 15;
+
+  } else if (this.size === "large"){
+    cost = size + 20;
+  } else {
+    alert("pick a size");
+  }
+  return this.total = fee;
+
+  // } else if (this.toppings.length > 2){
+  //
+  //   var price = size + price + toppings;
+  // } else if (this.toppings.length < 2){
+  //
+  //   var price = size + price + toppings;
+  // }
+
+  console.log(fee);
+};
+
+
 
 
 
 // Frontend Logic
 $(document).ready(function(){
-  $("form#form").submit(function(event){
+  $("#form").submit(function(event){
     event.preventDefault();
 
     let newSize = parseInt($("#size"));
     let newTopp = parseInt($("#toppings"));
 
+    let pizzaPrice = new Pizza(size, toppings, price);
+    pizzaPrice.getFee();
+console.log(Pizza);
 
 
 
